2f913b9274b3b2ea70bf04fe77ecb5da73c96113
Ritik Kumar <rk7382687@gmail.com>
2025-01-03 19:58:19 +0530

    video stream api

--- stream-front-end/src/App.jsx
+++ stream-front-end/src/App.jsx
@@ -1,8 +1,11 @@
-import { Toast } from "flowbite-react";
 import { VideoUpload } from "./components/VideoUpload";
 import { Toaster } from "react-hot-toast";
+import React, { useState } from "react";
 
 function App() {
+  const [videoId, setVideoId] = useState(
+    "900b73ec-4173-4e15-b21a-ff07fea3365c"
+  );
   return (
     <>
       <Toaster />
@@ -10,7 +13,47 @@ function App() {
         <h1 className=" text-gray-700 font-bold p=4 text-2xl  dark:text-gray-100">
           Video Streaming App
         </h1>
-        <VideoUpload />
+
+        <div className="flex mt-14 w-full justify-around">
+          <div>
+            <h1 className="text-white">Playing Video</h1>
+            <video
+              style={{
+                width: 500,
+              }}
+              src={`http://localhost:8080/api/v1/videos/stream/range/${videoId}`}
+              controls
+            ></video>
+
+            {/* <video
+              id="my-video"
+              class="video-js"
+              controls
+              preload="auto"
+              width="640"
+              height="264"
+              // poster="MY_VIDEO_POSTER.jpg"
+              data-setup="{}"
+            >
+              <source
+                src={`http://localhost:8080/api/v1/videos/stream/range/${videoId}`}
+                type="video/mp4"
+              />
+              <p class="vjs-no-js">
+                To view this video please enable JavaScript, and consider
+                upgrading to a web browser that
+                <a
+                  href="https://videojs.com/html5-video-support/"
+                  target="_blank"
+                >
+                  supports HTML5 video
+                </a>
+              </p>
+            </video> */}
+          </div>
+
+          <VideoUpload />
+        </div>
       </div>
     </>
   );
